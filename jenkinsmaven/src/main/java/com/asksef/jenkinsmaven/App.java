bdf3bedf50adbd232bbf08c89fb168a9aab52afa
Kwabena S <kwabenasefa@hotmail.com>
2022-08-07 00:25:06 -0400

    commit base

---
+++ jenkinsmaven/src/main/java/com/asksef/jenkinsmaven/App.java
@@ -0,0 +1,13 @@
+package com.asksef.jenkinsmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
